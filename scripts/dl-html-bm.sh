5bf8235fea7eb96fed30e64b95bd61d995c700dc
rootflags <linuxgod@gmail.com>
2021-12-22 16:51:14 -0600

    Bug fix when the PDF file is actually an HTML with JS

--- scripts/dl-html-bm.sh
+++ scripts/dl-html-bm.sh
@@ -16,3 +16,22 @@ FOLDER="/t3Portal/external/en/cr/BM${i}"
 
 ${SCRIPT_BASE}/dl-html-inc.sh "${FOLDER}"
 
+# Sometimes the PDF files are really an HTML redirect inside a Javascript bit.  In that case we'll re-process
+# the "PDF" files to download the correct PDF file if they're not really a PDF
+cd $DOWNLOAD_TO
+FOLDER="t3Portal/document/cr/BM${i}"
+find ${FSM_URLBASE}/${FOLDER} -name "*.pdf" | while read -r FILE
+do
+	echo PROCESSING $FILE
+	file "$FILE" | grep HTML> /dev/null 2>&1
+	if [ $? = 0 ]; then
+		PDF=`cat $FILE |sed -e 's/\.pdf.*$//g'|awk -F\' '{print $NF}'`
+		$WG -q ${WEBSITE}${PDF}.pdf\?sisuffix=ff\&locale=en\&siid=1640203137875
+		file "${FSM_URLBASE}/${PDF}.pdf?sisuffix=ff&locale=en&siid=1640203137875" | grep PDF
+		if [ $? = 0 ]; then
+			echo MOVING mv -f "${FSM_URLBASE}/${PDF}.pdf?sisuffix=ff&locale=en&siid=1640203137875" "$FILE"
+			rm -f "$FILE"
+			mv -f "${FSM_URLBASE}/${PDF}.pdf?sisuffix=ff&locale=en&siid=1640203137875" "$FILE"
+		fi
+	fi
+done
