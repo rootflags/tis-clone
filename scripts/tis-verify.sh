9b9575e3a042d2fff8cc4e8d4d2caee382a1ab20
rootflags <linuxgod@gmail.com>
2021-12-27 16:15:17 -0600

    Adding additional verification

--- scripts/tis-verify.sh
+++ scripts/tis-verify.sh
@@ -10,4 +10,22 @@ if [ x$1 = x ]; then
 else
 	cd $1
 fi
-grep -r "TIS is your service support source" * 
+
+if [ ! -d "t3Portal" ]; then
+	echo "You must run this from the root where folder t3Portal is located"
+	exit
+fi
+
+echo "Checking if any XHTML files listed in any toc.xml are missing"
+for TOC in `find . -name toc.xml`; do
+	for F in `cat $TOC|grep href=.*xhtml|sed -e 's/">.*$//g'|awk -F\" '{print $NF}'`; do
+		if [ ! -f ${PWD}${F} ]; then
+			echo Missing: ${PWD}${F}
+		fi
+	done
+done
+
+# Look for pages where download failed
+echo "Checking if any downloaded files failed due to expired or invalid cookies..."
+grep -r "TIS is your service support source" * |cut -d: -f1
+
